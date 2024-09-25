f588e2a9ddce704961328dee8fe83cabea89fedf
Noah Kawleski <noahkawleski@gmail.com>
2024-09-25 13:41:00 -0500

    added Noah's C# testing project

---
+++ C#Testing/testing/EntitiesFolder/Arc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+public class Arc : Entity
+{
+    Arc()
+    {
+        entityType = PossibleEntityTypes.arc;
+    }
+}
---
+++ C#Testing/testing/FileTypeFolder/SupportedFile.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Abstract class to be inherrited by every File child class
+ * - DWG, DXF, PDF
+ */
+using System;
+using System.IO;
+using System.Numerics;
+
+abstract public class SupportedFile
+{
+	protected string path;
+	protected SupportedExtensions fileType;
+	protected FeatureListClass featureList;
+	protected enum SupportedExtensions
+	{
+		pdf,
+		dwg,
+		dxf,
+	}
+	public SupportedFile(string path)
+	{
+		this.path = path;
+	}
+	public void setPath(string path)
+	{
+		this.path = path;
+	}
+	public string getPath()
+	{
+		return this.path;
+	}
+	public string getFileType()
+	{
+		return fileType.ToString();
+	}
+	public void writeFeatures()
+	{
+		File.WriteAllLines("features.txt", featureList.toStringArray());
+	}
+	public void readFeatures()
+	{
+		featureList.setFeatureList(File.ReadAllLines("features.txt"));
+	}
+	public FeatureListClass getFeatureList()
+	{
+		return featureList;
+	}
+	
+	/* 
+	 * Method that should be implemented by each child 
+	 * This is where the feature recognition logic will go
+	*/
+	abstract public bool findFeatures();
+}
