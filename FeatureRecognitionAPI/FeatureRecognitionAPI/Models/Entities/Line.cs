9e709e960defbffa0c78c4bf0d3aeae73a76661a
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2024-09-27 16:26:07 -0500

    Implemented the Line entity

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Line.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Line.cs
@@ -6,9 +6,26 @@ namespace FeatureRecognitionAPI.Models
 {
     public class Line : Entity
     {
-        Line()
+        public double StartX { get; set; }
+        public double StartY { get; set; }
+        public double EndX { get; set; }
+        public double EndY { get; set; }
+
+        public double Length { get; } 
+        private Line()
         {
             entityType = PossibleEntityTypes.line;
+        }
+
+        public Line(double startX, double startY, double endX, double endY)
+        {
+            StartX = startX;
+            StartY = startY;
+            EndX = endX;
+            EndY = endY;
+            
+            // Distance Calculation
+            Length = Math.Sqrt(Math.Pow(endX - startX, 2) + Math.Pow(endY - startY, 2));
         }
     }
 }
