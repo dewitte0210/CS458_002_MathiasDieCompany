eb71bad4dfcedebc200b5965b119faa61ee681c5
Stephen Ice <stephen2ice@gmail.com>
2024-09-29 18:44:21 -0500

    Updated the following: EntityListClass.cs - created constructor and addEntity() function Line, Arc and Circle.cs - implemented constructor and most basic data fields DXFFile.cs - implemented constructor, implemented readEntities()

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Arc.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Arc.cs
@@ -4,8 +4,20 @@ using System.Numerics;
 
 public class Arc : Entity
 {
-    Arc()
+    private decimal xPoint, yPoint, radius, startAngle, endAngle;
+
+    private Arc()
+    {
+        //left empty because private (unusable)
+    }
+
+    public Arc(decimal xPoint, decimal yPoint, decimal radius, decimal startAngle, decimal endAngle)
     {
         entityType = PossibleEntityTypes.arc;
+        this.xPoint=xPoint;
+        this.yPoint=yPoint;
+        this.radius=radius;
+        this.startAngle=startAngle;
+        this.endAngle=endAngle;
     }
 }
--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/EntityListClass.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/EntityListClass.cs
@@ -5,9 +5,14 @@ using System;
 
 public class EntityListClass
 {
-    Entity[] entityList;
+    private Entity[] entityList;
+    //Refers to to the current index at the 'end' of the entityList
+    private int entityIndex;
     public EntityListClass()
     {
+        //set entity index to -1 to indicate empty list
+        entityIndex = -1;
+        entityList = new Entity[100];
     }
 
     //Convert entityList to a readable array to be printed in a file
@@ -22,4 +27,32 @@ public class EntityListClass
     {
 
     }
+
+    public bool addEntity(Entity entity)
+    {
+        //If there is room in the array, add the entity
+        if (entityIndex < entityList.Length)
+        {
+            entityIndex++;
+            entityList[entityIndex] = entity;
+            return true;
+        }
+        //If there is not room, double entity array size
+        else if (entityIndex >= entityList.Length)
+        {
+            Entity[] temp = new Entity[(entityList.Length * 2)];
+
+            for (int i = 0; i < entityList.Length; i++)
+            {
+                temp[i] = entityList[i];
+            }
+            entityList = temp;
+            //Add Entity
+            entityIndex++;
+            entityList[entityIndex] = entity;
+            return true;
+        }
+        return false;
+    }
+
 }
