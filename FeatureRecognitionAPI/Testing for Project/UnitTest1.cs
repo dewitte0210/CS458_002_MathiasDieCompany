0caa26843afbf1b3817d92a7a6bf1abacccaf45a
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2024-09-30 15:25:22 -0500

    Suppressing test warnings

--- FeatureRecognitionAPI/Testing for Project/UnitTest1.cs
+++ FeatureRecognitionAPI/Testing for Project/UnitTest1.cs
@@ -15,36 +15,36 @@ namespace Testing_for_Project
         public void TestLineClass()
         {
             Line line1 = new Line(1.9724753611322341m, 0.4071346127707478m, 2.6124753611322347m, 0.4071346127707472m);
-            Assert.AreEqual(1.9724753611322341m, line1.StartX);
-            Assert.AreEqual(0.4071346127707478m, line1.StartY);
-            Assert.AreEqual(2.6124753611322347m, line1.EndX);
-            Assert.AreEqual(0.4071346127707472m, line1.EndY);
-            Assert.AreEqual(DecimalEx.Sqrt(DecimalEx.Pow(2.6124753611322347m - 1.9724753611322341m, 2) + DecimalEx.Pow(0.4071346127707472m - 0.4071346127707478m, 2)), line1.Length);
+            Assert.That(line1.StartX, Is.EqualTo(1.9724753611322341m));
+            Assert.That(line1.StartY, Is.EqualTo(0.4071346127707478m));
+            Assert.That(line1.EndX, Is.EqualTo(2.6124753611322347m));
+            Assert.That(line1.EndY, Is.EqualTo(0.4071346127707472m));
+            Assert.That(line1.Length, Is.EqualTo(DecimalEx.Sqrt(DecimalEx.Pow(2.6124753611322347m - 1.9724753611322341m, 2) + DecimalEx.Pow(0.4071346127707472m - 0.4071346127707478m, 2))));
         }
         [Test]
         public void TestArcClass()
         {
             Arc arc1 = new Arc(2.6124753611322347m, 0.4371346127707472m, 0.03m, 270m, 0.0000000000002120m);
-            Assert.AreEqual((0.03m * DecimalEx.Cos(270m * DecimalEx.Pi / 180m) + 2.6124753611322347m), arc1.startX);
-            Assert.AreEqual((0.03m * DecimalEx.Sin(270m * DecimalEx.Pi / 180m) + 0.4371346127707472m), arc1.startY);
-            Assert.AreEqual((0.03m * DecimalEx.Cos(0.0000000000002120m * DecimalEx.Pi / 180m) + 2.6124753611322347m), arc1.endX);
-            Assert.AreEqual((0.03m * DecimalEx.Sin(0.0000000000002120m * DecimalEx.Pi / 180m) + 0.4371346127707472m), arc1.endY);
-            Assert.AreEqual(0.03m, arc1.radius);
-            Assert.AreEqual(2.6124753611322347m, arc1.centerX);
-            Assert.AreEqual(0.4371346127707472m, arc1.centerY);
-            Assert.AreEqual(270m, arc1.startAngle);
-            Assert.AreEqual(0.0000000000002120m, arc1.endAngle);
-            Assert.AreEqual(0.0000000000002120m - 270m + 360, arc1.centralAngle);
-            Assert.AreEqual(2 * DecimalEx.Pi * 0.03m * ((0.0000000000002120m - 270m + 360m) / 360), arc1.length);
+            Assert.That(arc1.startX, Is.EqualTo((0.03m * DecimalEx.Cos(270m * DecimalEx.Pi / 180m) + 2.6124753611322347m)));
+            Assert.That(arc1.startY, Is.EqualTo((0.03m * DecimalEx.Sin(270m * DecimalEx.Pi / 180m) + 0.4371346127707472m)));
+            Assert.That(arc1.endX, Is.EqualTo((0.03m * DecimalEx.Cos(0.0000000000002120m * DecimalEx.Pi / 180m) + 2.6124753611322347m)));
+            Assert.That(arc1.endY, Is.EqualTo((0.03m * DecimalEx.Sin(0.0000000000002120m * DecimalEx.Pi / 180m) + 0.4371346127707472m)));
+            Assert.That(arc1.radius, Is.EqualTo(0.03m));
+            Assert.That(arc1.centerX, Is.EqualTo(2.6124753611322347m));
+            Assert.That(arc1.centerY, Is.EqualTo(0.4371346127707472m));
+            Assert.That(arc1.startAngle, Is.EqualTo(270m));
+            Assert.That(arc1.endAngle, Is.EqualTo(0.0000000000002120m));
+            Assert.That(arc1.centralAngle, Is.EqualTo(0.0000000000002120m - 270m + 360));
+            Assert.That(arc1.length, Is.EqualTo(2 * DecimalEx.Pi * 0.03m * ((0.0000000000002120m - 270m + 360m) / 360)));
         }
         [Test]
         public void TestCircleClass()
         {
             Circle circle1 = new Circle(0.7124999999999999m, 0.7124999999999999m, 0.2577228596164672m);
-            Assert.AreEqual(0.7124999999999999m, circle1.centerX);
-            Assert.AreEqual(0.7124999999999999m, circle1.centerY);
-            Assert.AreEqual(0.2577228596164672m, circle1.radius);
-            Assert.AreEqual((2 * DecimalEx.Pi * 0.2577228596164672m), circle1.perimeter);
+            Assert.That(circle1.centerX, Is.EqualTo(0.7124999999999999m));
+            Assert.That(circle1.centerY, Is.EqualTo(0.7124999999999999m));
+            Assert.That(circle1.radius, Is.EqualTo(0.2577228596164672m));
+            Assert.That(circle1.perimeter, Is.EqualTo((2 * DecimalEx.Pi * 0.2577228596164672m)));
         }
     }
 }
