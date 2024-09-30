71aaf80d2d183e128e6f5d87df4c7bf83c03b85a
kruegerj9819 <144365600+kruegerj9819@users.noreply.github.com>
2024-09-30 12:37:16 -0500

    Circle class and unit tests for remaining classes

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Circle.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Circle.cs
@@ -5,9 +5,24 @@ using System.Numerics;
 namespace FeatureRecognitionAPI.Models { 
     public class Circle : Entity
     {
-        Circle()
+        //Center point of circle - x value
+        public double centerX {  get; set; }
+        //Center point of circle - y value
+        public double centerY { get; set; }
+        public double radius { get; set; }
+        public double perimeter { get; }
+        public Circle(double centerX, double centerY, double radius)
         {
             entityType = PossibleEntityTypes.circle;
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.radius = radius;
+            this.perimeter = calcPerimeter(radius);
+        }
+
+        private double calcPerimeter(double radius)
+        {
+            return 2 * Math.PI * radius;
         }
     }
 }
