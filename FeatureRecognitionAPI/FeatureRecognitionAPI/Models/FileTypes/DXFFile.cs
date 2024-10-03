322a4fc83d3c50ab0f3fbe8b58a359d492eed55d
Stephen Ice <stephen2ice@gmail.com>
2024-10-02 19:08:46 -0500

    Updated DXFFile to be modular again with new logic

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/FileTypes/DXFFile.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/FileTypes/DXFFile.cs
@@ -37,6 +37,39 @@ namespace FeatureRecognitionAPI.Models
             string[] lines = File.ReadAllLines(path);
 
             //find and track index where entities begin in file (where parsing into entities starts)
+            int index = GetStartIndex(lines);
+
+            //While we haven't reached the end of the entities section, loop and grab entities
+            while ((lines[index] != "ENDSEC") && (lines.Length > index))
+            {
+
+                index++;
+                //Console.WriteLine("In while loop: " + lines[index]);
+
+                switch (lines[index])
+                {
+                    case "LINE":
+
+                        index = ParseLine(lines, index);
+                        break;
+
+                    case "ARC":
+                        index = ParseArc(lines, index);
+                        break;
+
+                    case "CIRCLE":
+                        index = ParseCircle(lines, index);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        private int GetStartIndex(string[] lines)
+        {
             int startIndex = 0;
             for (int i = 0; i < lines.Length; i++)
             {
@@ -48,162 +81,152 @@ namespace FeatureRecognitionAPI.Models
                     break;
                 }
             }
+            return startIndex;
+        }
 
-            int index = startIndex;
-
-            //Console.WriteLine("At the start of the while loop");
-            //While we haven't reached the end of the entities section, loop and grab entities
-            while ((lines[index] != "ENDSEC") && (lines.Length > index))
+        private int ParseLine(string[] lines, int index)
+        {
+            //Variables used to create entity of type Line
+            decimal xStart = 0;
+            decimal yStart = 0;
+            decimal xEnd = 0;
+            decimal yEnd = 0;
+            //Index must be incremented along with i in order to keep maintain accurate indexing
+            while (lines[index] != "  0")
             {
-
-                index++;
-                //Console.WriteLine("In while loop: " + lines[index]);
-
+                //Switch based on which data field is being interpreted
                 switch (lines[index])
                 {
-                    case "LINE":
-                        //Variables used to create entity of type Line
-                        decimal xStart = 0;
-                        decimal yStart = 0;
-                        decimal xEnd = 0;
-                        decimal yEnd = 0;
-                        //Index must be incremented along with i in order to keep maintain accurate indexing
-                        while (lines[index] != "  0")
-                        {
-                            //Switch based on which data field is being interpreted
-                            switch (lines[index])
-                            {
 
-                                case " 10":
-                                    index++;
-                                    xStart = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(xStart + " xStart");
-                                    break;
-
-                                case " 11":
-                                    index++;
-                                    xEnd = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(xEnd + " xEnd");
-                                    break;
-
-                                case " 20":
-                                    index++;
-                                    yStart = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(yStart +" yStart");
-                                    break;
-
-                                case " 21":
-                                    index++;
-                                    yEnd = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(yEnd + " yEnd");
-                                    break;
-
-                                default:
-                                    break;
-
-                            }
-                            index++;
-
-                        }
-                        Line lineEntity = new Line(xStart, yStart, xEnd, yEnd);
-                        entityList.Add(lineEntity);
+                    case " 10":
+                        index++;
+                        xStart = decimal.Parse(lines[index]);
+                        //Console.WriteLine(xStart + " xStart");
                         break;
 
-                    case "ARC":
-                        decimal xPoint = 0;
-                        decimal yPoint = 0;
-                        decimal radius = 0;
-                        decimal startAngle = 0;
-                        decimal endAngle = 0;
-
-                        while (lines[index] != "  0")
-                        {
-                            switch (lines[index])
-                            {
-
-                                case " 10":
-                                    index++;
-                                    xPoint = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(xPoint + " xPoint");
-                                    break;
-
-                                case " 20":
-                                    index++;
-                                    yPoint = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(yPoint + " yPoint");
-                                    break;
-
-                                case " 40":
-                                    index++;
-                                    radius = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(radius +" yStart");
-                                    break;
-
-                                case " 50":
-                                    index++;
-                                    startAngle = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(startAngle + " startAngle");
-                                    break;
-
-                                case " 51":
-                                    index++;
-                                    endAngle = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(endAngle + " endAngle");
-                                    break;
-
-                                default:
-                                    break;
-
-                            }
-                            index++;
-                        }
-                        Arc arcEntity = new Arc(xPoint, yPoint, radius, startAngle, endAngle);
-                        entityList.Add(arcEntity);
+                    case " 11":
+                        index++;
+                        xEnd = decimal.Parse(lines[index]);
+                        //Console.WriteLine(xEnd + " xEnd");
                         break;
 
-                    case "CIRCLE":
-                        xPoint = 0;
-                        yPoint = 0;
-                        radius = 0;
-                        while (lines[index] != "  0")
-                        {
-                            switch (lines[index])
-                            {
+                    case " 20":
+                        index++;
+                        yStart = decimal.Parse(lines[index]);
+                        //Console.WriteLine(yStart +" yStart");
+                        break;
 
-                                case " 10":
-                                    index++;
-                                    xPoint = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(xPoint + " xPoint");
-                                    break;
-
-                                case " 20":
-                                    index++;
-                                    yPoint = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(yPoint + " yPoint");
-                                    break;
-
-                                case " 40":
-                                    index++;
-                                    radius = decimal.Parse(lines[index]);
-                                    //Console.WriteLine(radius +" yStart");
-                                    break;
-
-                                default:
-                                    break;
-
-                            }
-                            index++;
-                        }
-                        Circle circleEntity = new Circle(xPoint, yPoint, radius);
-                        entityList.Add(circleEntity);
+                    case " 21":
+                        index++;
+                        yEnd = decimal.Parse(lines[index]);
+                        //Console.WriteLine(yEnd + " yEnd");
                         break;
 
                     default:
                         break;
+
                 }
+                index++;
 
             }
+            Line lineEntity = new Line(xStart, yStart, xEnd, yEnd);
+            entityList.Add(lineEntity);
+            return index;
         }
+
+        private int ParseArc(string[] lines, int index)
+        {
+            decimal xPoint = 0;
+            decimal yPoint = 0;
+            decimal radius = 0;
+            decimal startAngle = 0;
+            decimal endAngle = 0;
+
+            while (lines[index] != "  0")
+            {
+                switch (lines[index])
+                {
+
+                    case " 10":
+                        index++;
+                        xPoint = decimal.Parse(lines[index]);
+                        //Console.WriteLine(xPoint + " xPoint");
+                        break;
+
+                    case " 20":
+                        index++;
+                        yPoint = decimal.Parse(lines[index]);
+                        //Console.WriteLine(yPoint + " yPoint");
+                        break;
+
+                    case " 40":
+                        index++;
+                        radius = decimal.Parse(lines[index]);
+                        //Console.WriteLine(radius +" yStart");
+                        break;
+
+                    case " 50":
+                        index++;
+                        startAngle = decimal.Parse(lines[index]);
+                        //Console.WriteLine(startAngle + " startAngle");
+                        break;
+
+                    case " 51":
+                        index++;
+                        endAngle = decimal.Parse(lines[index]);
+                        //Console.WriteLine(endAngle + " endAngle");
+                        break;
+
+                    default:
+                        break;
+
+                }
+                index++;
+            }
+            Arc arcEntity = new Arc(xPoint, yPoint, radius, startAngle, endAngle);
+            entityList.Add(arcEntity);
+            return index;
+        }
+
+        private int ParseCircle(string[] lines, int index)
+        {
+            decimal xPoint = 0;
+            decimal yPoint = 0;
+            decimal radius = 0;
+            while (lines[index] != "  0")
+            {
+                switch (lines[index])
+                {
+
+                    case " 10":
+                        index++;
+                        xPoint = decimal.Parse(lines[index]);
+                        //Console.WriteLine(xPoint + " xPoint");
+                        break;
+
+                    case " 20":
+                        index++;
+                        yPoint = decimal.Parse(lines[index]);
+                        //Console.WriteLine(yPoint + " yPoint");
+                        break;
+
+                    case " 40":
+                        index++;
+                        radius = decimal.Parse(lines[index]);
+                        //Console.WriteLine(radius +" yStart");
+                        break;
+
+                    default:
+                        break;
+
+                }
+                index++;
+            }
+            Circle circleEntity = new Circle(xPoint, yPoint, radius);
+            entityList.Add(circleEntity);
+            return index;
+        }
+
         //May need to be refactored depending on of c# handles this by copy or by reference
         public List<Entity> GetEntities()
         {
