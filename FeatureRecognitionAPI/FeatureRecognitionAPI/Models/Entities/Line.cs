4cdff28ebef16df7eabd930cc450ec4a97d49050
Noah Kawleski <noahkawleski@gmail.com>
2024-10-23 15:21:09 -0500

    Revert "Fixed merge conflicts"
    This reverts commit 36a27c98b784057975a933e8651e7ba009882190.

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Line.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Entities/Line.cs
@@ -54,7 +54,7 @@ namespace FeatureRecognitionAPI.Models
             SlopeX = EndX - StartX;
 
             // Distance Calculation
-            this.setLength( Math.Sqrt(Math.Pow(endX - startX, 2) + Math.Pow(endY - startY, 2)));
+            Length = Math.Sqrt(Math.Pow(endX - startX, 2) + Math.Pow(endY - startY, 2));
         }
 
         public bool isParallel(Entity other)
