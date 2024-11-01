006278d2d8e4e1b15e0628e6cbbd75c8d13b5ad5
Stephen Ice <74511023+Si-private@users.noreply.github.com>
2024-11-01 14:40:24 -0500

    Started working on FeatureGroups

---
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Features/FeatureGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace FeatureRecognitionAPI.Models.Features
+{
+    public class FeatureGroup
+    {
+        protected int count;
+        protected List<Feature> features;
+        public FeatureGroup(int count, List<Feature> features) 
+        {
+            this.count = count;
+            this.features = features;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
