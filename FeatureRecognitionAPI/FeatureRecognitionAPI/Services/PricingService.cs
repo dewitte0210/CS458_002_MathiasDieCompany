3429a06bce1558adfac585168459dd6e0a194f6d
pseudoRiley <rcwuest329@gmail.com>
2024-11-20 16:14:38 -0600

    Started logic

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Services/PricingService.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Services/PricingService.cs
@@ -16,20 +16,66 @@ namespace FeatureRecognitionAPI.Services
         {
             try
             {
-                double runningCost = 0;
-
-                //Calculate total perimeter cost
+                double totalEstimate = 0.00;
+                double totalPerimeter = 0;
 
                 foreach (var feature in param.Features)
                 {
+                    double setupCost = 0;
+                    double oneUpCost = 0;
+                    double featureCost = 0;
+                    bool isOverSized = feature.Perimeter > 20 ? true : false;
+                    int quantity = feature.Count;
+                    if (quantity > 500)
+                    {
+                        //TODO: Need to add some sort of flag to indicate that the quantity is too high
+                    }
+                    double quantityFactor = GetQuantityFactor(quantity);
+                    
 
+                    totalPerimeter += (feature.Perimeter * quantity);
+
+                    if (feature.KissCut)
+                    {
+                        setupCost = setupCost * 1.15;
+                        oneUpCost = oneUpCost * 1.15;
+                    }
+
+                    if (isOverSized)
+                    {
+
+                    }
+
+                    featureCost = setupCost + (oneUpCost * quantity);
+                    totalEstimate += featureCost;
                 }
-                return (OperationStatus.OK, "Successfully estimated price", "1354.36");
+
+                return (OperationStatus.OK, "Successfully estimated price", totalEstimate.ToString("c"));
             }
             catch (Exception ex)
             {
                 return (OperationStatus.ExternalApiFailure, ex.Message, null);
             }
+        }
+
+        private double GetQuantityFactor(int quantity)
+        {
+            if (quantity >= 1 && quantity <= 12)
+                return 1;
+            else if (quantity >= 13 && quantity <= 24)
+                return .93;
+            else if (quantity >= 25 && quantity <= 49)
+                return .85;
+            else if (quantity >= 50 && quantity <= 99)
+                return .79;
+            else if (quantity >= 100 && quantity <= 199)
+                return .71;
+            else if (quantity >= 200 && quantity <= 499)
+                return .64;
+            else if (quantity >= 500)
+                return .63;
+
+
         }
     }
 }
