abba5a672e961457fb6aa0e9506e0e4e079deda5
Andrew <schmidta0742@my.uwstout.edu>
2025-02-27 17:18:05 -0600

    updated angle calculations and added radians and degrees class

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Utility/Angles.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Utility/Angles.cs
@@ -1,27 +1,21 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Dynamic;
-//using System.Linq;
-//using System.Runtime.ExceptionServices;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//using FeatureRecognitionAPI.Models.Entities;
-
-//TODO: overload cross and dot product instead of having different for lines and vectors
-//TODO: remove classes like line that are already in project/library
-//TODO: make angle class more usable
-//TODO: convert to best practices
 //TODO: verify that getMinorAngle always calculates interior angle and rename
-//TODO: clean up comments
-//TODO: document methods
-//TODO: verify that polygon members stay up to date
+//TODO: decide what to do with polygon class
 
-// Author: Andrew Schmidt
+/// Author: Andrew Schmidt
+/// <summary>
+/// This file is used for calculating the angle between lines and on what side they lay
+/// </summary>
 namespace FeatureRecognitionAPI.Models.Utility
 {
+    /// <summary>
+    /// Angles contains classes and functions for calculating angles
+    /// Not to be used directly, use Angle class, Side and Orientation enums, etc
+    /// </summary>
     class Angles
     {
+        /// <summary>
+        /// The side an angle lies in relation to the rest of the shape
+        /// </summary>
         public enum Side
         {
             INTERIOR,
@@ -29,6 +23,11 @@ namespace FeatureRecognitionAPI.Models.Utility
             UNKNOWN
         }
 
+        /// <summary>
+        /// The general orientation of a polygon
+        /// This is the direction its lines are drawn
+        /// Ideally all lines of a polygon should have a consistent direction to them
+        /// </summary>
         public enum Orientation
         {
             COUNTERCLOCKWISE,
@@ -36,92 +35,93 @@ namespace FeatureRecognitionAPI.Models.Utility
             UNKNOWN
         }
 
+        public class Degrees(double angle)
+        {
+            public double angle = angle;
+
+            public Degrees GetOppositeAngle()
+            {
+                return new Degrees(360 - angle);
+            }
+
+            public void SetToOpposite()
+            {
+                angle = 360 - angle;
+            }
+
+            public Radians ToRadians()
+            {
+                return new Radians(angle * Math.PI / 180);
+            }
+            
+        }
+
+        public class Radians(double angle)
+        {
+            public double angle = angle;
+
+            public Radians GetOppositeAngle()
+            {
+                return new Radians((2 * Math.PI) - angle);
+            }
+
+            public void SetToOpposite()
+            {
+                angle = (2 * Math.PI) - angle;
+            }
+
+            public Degrees ToDegrees()
+            {
+                return new Degrees(angle * 180 / Math.PI);
+            }
+
+        }
+
+        /// <summary>
+        /// The angle and side that angle is on between two lines
+        /// </summary>
         public class Angle
         {
-            private double degrees;
-            public Side side;
+            private readonly Degrees angle;
+            private readonly Side side;
 
-            public Angle(double newDegrees, Side newSide)
+            public Angle(Degrees angle, Side side)
             {
-                degrees = newDegrees;
-                side = newSide;
+                this.angle = angle;
+                this.side = side;
             }
 
-            public double getDegrees()
+            public Angle(Radians angle, Side side)
             {
-                return degrees;
+                this.angle = angle.ToDegrees();
+                this.side = side;
             }
 
-            public double getRadians()
+            public Degrees GetDegrees()
             {
-                return degrees * (Math.PI / 180);
+                return angle;
+            }
+
+            public Radians GetRadians()
+            {
+                return angle.ToRadians();
+            }
+
+            public Side GetSide()
+            {
+                return side;
             }
         }
 
-        //public class Point
-        //{
-        //    public double x;
-        //    public double y;
 
-        //    public Point(double newX, double newY)
-        //    {
-        //        x = newX;
-        //        y = newY;
-        //    }
-        //}
-
-        //public class ThreePoint
-        //{
-        //    public bool valid;
-
-        //    public Point a;
-        //    public Point b;
-        //    public Point c;
-
-        //    public ThreePoint(Point newA, Point newB, Point newC)
-        //    {
-        //        a = newA;
-        //        b = newB;
-        //        c = newC;
-        //    }
-        //}
-
-        //public class Line
-        //{
-        //    public Point start;
-        //    public Point end;
-
-        //    public Line(Point newStart, Point newEnd)
-        //    {
-        //        start = newStart;
-        //        end = newEnd;
-        //    }
-
-        //    public Point getRelative()
-        //    {
-        //        return new Point(end.x - start.x, end.y - start.y);
-        //    }
-
-        //    public double getLength()
-        //    {
-        //        return Math.Sqrt(Math.Pow(getRelative().x, 2) + Math.Pow(getRelative().y, 2));
-        //    }
-        //}
-
-        //confirm that this math is correct
-
-        //cannot guarantee this way will work with lines
-        //may need to translate the two lines to two points??
         public static double CrossProduct(Point a, Point b)
         {
-            //Console.WriteLine(a.getRelative().x + " " + a.getRelative().y + " " + b.getRelative().x + " " + b.getRelative().y);
-            //Console.WriteLine(a.getRelative().x * b.getRelative().y - a.getRelative().y * b.getRelative().x);
-            //return a.getRelative().x * b.getRelative().y - a.getRelative().y * b.getRelative().x;
             return a.X * b.Y - a.Y * b.X;
         }
 
-        //get the cross product of two lines
-        //uses the delta of the two given lines
+        /// <summary>
+        /// Calculates the cross product of two lines using their deltas
+        /// </summary>
         public static double CrossProduct(Line a, Line b)
         {
             Point aDelta = a.GetDelta();
@@ -131,80 +131,121 @@ namespace FeatureRecognitionAPI.Models.Utility
 
         public static double DotProduct(Point a, Point b)
         {
-            //return a.getRelative().x * b.getRelative().x + a.getRelative().y * b.getRelative().y;
-            //return a.start.x * b.start.x + a.start.y * b.start.y;
             return a.X * b.X + a.Y * b.Y;
         }
 
+        /// <summary>
+        /// Calculates the dot product of two lines using their deltas
+        /// </summary>
         public static double DotProduct(Line a, Line b)
         {
             Point aDelta = a.GetDelta();
             Point bDelta = b.GetDelta();
             return aDelta.X * bDelta.X + aDelta.Y * bDelta.Y;
-
-            //return a.getRelative().x * b.getRelative().x + a.getRelative().y * b.getRelative().y;
-            //return a.start.x * b.start.x + a.start.y * b.start.y;
         }
 
-        //public static ThreePoint getThreePoint(Line a, Line b)
-        //{
-        //    ThreePoint tp = new ThreePoint(new Point(0, 0), new Point(0, 0), new Point(0, 0));
-        //    tp.valid = false;
-
-        //    tp.a = a.start;
-        //    tp.c = b.end;
-
-        //    //shared start and end
-        //    if (a.end == b.start)
-        //    {
-        //        tp.b = a.end;
-        //        tp.valid = true;
-        //    }
-        //    //lines intersect at some point
-        //    else if (true)
-        //    {
-        //        Console.WriteLine("lines do not touch, need to handle this case");
-        //    }
-        //    //otherwise lines are parallel
-
-        //    return tp;
-        //}
-
-        public static Angle GetMinorAngle(Line a, Line b)
+        // may not handle all cases yet, to be tested
+        public static Angle GetAngle(Line a, Line b, Side targetSide = Side.INTERIOR)
         {
             double cross = CrossProduct(a, b);
             double dot = DotProduct(a, b);
+            Side side = Side.UNKNOWN;
 
             double cos_theta = dot / (a.Length * b.Length);
             cos_theta = Math.Max(-1, Math.Min(1, cos_theta));
 
-            double angle = 0;
-            angle = Math.Acos(cos_theta) * (180 / Math.PI);
+            double angle = Math.Acos(cos_theta) * (180 / Math.PI);
 
             if (cross < 0)
             {
                 angle = 360 - angle;
             }
 
-            //need to verify that 180 minus angle gives right thing
-            return new Angle(Math.Abs(180 - angle), Side.UNKNOWN);
-        }
-
-        public Side GetAngleDirection(Line a, Line b)
-        {
-            double cross = CrossProduct(a, b);
-
             if (cross > 0)
             {
-                return Side.INTERIOR;
+                side = Side.INTERIOR;
             }
             else if (cross < 0)
             {
-                return Side.EXTERIOR;
+                side = Side.EXTERIOR;
+            }
+
+            Degrees returnAngle = new Degrees(Math.Abs(180 - angle));
+            if (side != Side.UNKNOWN && side != targetSide)
+            {
+                returnAngle.SetToOpposite();
+            }
+
+            return new Angle(returnAngle, side);
+        }
+
+        /// <summary>
+        /// Checks if a polygon is closed.
+        /// The polygon must be entirely made of lines.
+        /// </summary>
+        /// <param name="lineList"></param>
+        /// <returns></returns>
+        public static bool PolygonIsClosed(List<Line> lineList)
+        {
+            //check if closed
+            for (int i = 0; i < lineList.Count; i++)
+            {
+                if (lineList[0].EndPoint != lineList[(i + 1) % lineList.Count].StartPoint)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Calculates the area of a polygon.
+        /// If the area is positive the area is counterclockwise.
+        /// Only works for polygons made entirely up of lines.
+        /// Make sure to do absolute value if all you need is the area.
+        /// </summary>
+        /// <param name="lineList"></param>
+        /// <returns></returns>
+        public static double GetShoelaceArea(List<Line> lineList)
+        {
+            if (!PolygonIsClosed(lineList))
+            {
+                return 0;
+            }
+
+            double area = 0;
+            for (int i = 0; i < lineList.Count; i++)
+            {
+                area += CrossProduct(lineList[i], lineList[(i + 1) % lineList.Count]);
+            }
+            if (area != 0)
+            {
+                area /= 2;
+            }
+            return area;
+        }
+
+        /// <summary>
+        /// Uses shoelace area to determine the orientation of a polygon.
+        /// The same conditions apply as GetShoelaceArea
+        /// </summary>
+        /// <param name="lineList"></param>
+        /// <returns></returns>
+        public static Orientation GetOrientation(List<Line> lineList)
+        {
+            double area = GetShoelaceArea(lineList);
+
+            if (area > 0)
+            {
+                return Orientation.COUNTERCLOCKWISE;
+            }
+            else if (area < 0)
+            {
+                return Orientation.CLOCKWISE;
             }
             else
             {
-                return Side.UNKNOWN;
+                return Orientation.UNKNOWN;
             }
         }
 
@@ -219,41 +260,12 @@ namespace FeatureRecognitionAPI.Models.Utility
             {
                 lineList = newLineList;
 
-                area = Math.Abs(GetShoelaceArea());
-                orientation = GetOrientation();
-            }
-            private double GetShoelaceArea()
-            {
-                double area = 0;
-                for (int i = 0; i < lineList.Count; i++)
-                {
-                    area += CrossProduct(lineList[i], lineList[(i + 1) % lineList.Count]);
-                }
-                if (area != 0)
-                {
-                    area /= 2;
-                }
-                return area;
-            }
-
-            private Orientation GetOrientation()
-            {
-                double area = GetShoelaceArea();
-
-                if (area > 0)
-                {
-                    return Orientation.COUNTERCLOCKWISE;
-                }
-                else if (area < 0)
-                {
-                    return Orientation.CLOCKWISE;
-                }
-                else
-                {
-                    return Orientation.UNKNOWN;
-                }
+                area = Math.Abs(GetShoelaceArea(lineList));
+                orientation = GetOrientation(lineList);
             }
         }
+    }
+}
 
     //    static void Main(string[] args)
     //    {
@@ -290,8 +302,6 @@ namespace FeatureRecognitionAPI.Models.Utility
 
     //        Console.Write("end");
     //    }
-    }
-}
 
 //cases to test
 // touching lines
