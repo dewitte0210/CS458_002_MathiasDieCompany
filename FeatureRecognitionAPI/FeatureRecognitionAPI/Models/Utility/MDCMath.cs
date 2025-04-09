2efdd646b6639b52a4012602c820e91b1be35a5d
Andrew <schmidta0742@my.uwstout.edu>
2025-04-09 13:24:15 -0500

    cleanup on models/utility folder

--- FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Utility/MDCMath.cs
+++ FeatureRecognitionAPI/FeatureRecognitionAPI/Models/Utility/MDCMath.cs
@@ -1,13 +1,13 @@
 namespace FeatureRecognitionAPI.Models.Utility
 {
-	public static class MDCMath
-	{
-		/// <summary>
-		/// Compare if two doubles are equal within a tolerance
-		/// </summary>
-		public static bool DoubleEquals(double a, double b, double tolerance = 0.001)
-		{
-			return (a > b - tolerance && a < b + tolerance);
-		}
-	}
+    public static class MdcMath
+    {
+        /// <summary>
+        /// Compare if two doubles are equal within a tolerance
+        /// </summary>
+        public static bool DoubleEquals(double a, double b, double tolerance = 0.001)
+        {
+            return (a > b - tolerance && a < b + tolerance);
+        }
+    }
 }
