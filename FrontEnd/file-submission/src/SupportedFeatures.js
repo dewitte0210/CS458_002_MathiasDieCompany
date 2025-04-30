6e997fd29989a9ee6ed1d035a546c5ff9b1b9746
kruegerj9819 <144365600+kruegerj9819@users.noreply.github.com>
2025-04-30 15:39:07 -0500

    Added unlisted supported features as well as high quality images for each feature group

--- FrontEnd/file-submission/src/SupportedFeatures.js
+++ FrontEnd/file-submission/src/SupportedFeatures.js
@@ -1,27 +1,27 @@
 ﻿export let supportedFeatures = [
     {
-        src: "/Grp1A.ico",
+        src: "/FeatureGroupImages/Group1A.png",
         alt: "Group1A",
         description: "Group 1A",
         longDescription:
             'Group 1A features are comprised of square and radius corner rectangles greater than 1/4" wide. This group is typically internal features and cavity perimeters.',
     },
     {
-        src: "/Grp1B.ico",
+        src: "/FeatureGroupImages/Group1B.png",
         alt: "Group1B",
         description: "Group 1B",
         longDescription:
             'Group 1B features are comprised of ruled circles and full radius obrounds greater than 3/8" wide. Circle cutouts less than 1 5/8" diameter are generally punches.',
     },
     {
-        src: "/Grp1C.ico",
+        src: "/FeatureGroupImages/Group1C.png",
         alt: "Group1C",
         description: "Group 1C",
         longDescription:
             'Group 1C features are comprised of mitered and radius corner triangles. Mitered triangles will typically cost more becaue of the hand grinding required to miter the cut edges correctly. Overall dimensions less than 3/8" should be reviewed.',
     },
     {
-        src: "/Grp2A.ico",
+        src: "/FeatureGroupImages/Group2A.png",
         alt: "Group2A",
         description: "Group 2A",
         longDescription:
@@ -29,14 +29,14 @@
             'Bowtie detection is not functioning at the moment',
     },
     {
-        src: "/Grp3.ico",
+        src: "/FeatureGroupImages/Group3.png",
         alt: "Group3",
         description: "Group 3",
         longDescription:
             "Group 3 is comprised of chamfered corners.",
     },
     {
-        src: "/Grp4.ico",
+        src: "/FeatureGroupImages/Group4.png",
         alt: "Group4",
         description: "Group 4",
         longDescription:
@@ -44,17 +44,52 @@
             " is working as of right now",
     },
     {
-        src: "/Grp5.ico",
+        src: "/FeatureGroupImages/Group5.png",
         alt: "Group5",
         description: "Group 5",
         longDescription:
             "Group 5 is comprised of mitered notches (perimeter features).",
     },
     {
-        src: "/Grp6.ico",
+        src: "/FeatureGroupImages/Group6.png",
         alt: "Group6",
         description: "Group 6",
         longDescription:
             "Group 6 is comprised of radius notches (perimeter features) and D-Subs with no tabs.",
+    },
+    {
+        src: "/FeatureGroupImages/Group9.png",
+        alt: "Group9",
+        description: "Group 9",
+        longDescription:
+            "Group 9 is comprised of kiss-cut tabs.",
+    },
+    {
+        src: "/FeatureGroupImages/Group10.png",
+        alt: "Group10",
+        description: "Group 10",
+        longDescription:
+            "Group 10 is comprised of a special internal feature found in some spacer layers and circuit dies.",
+    },
+    {
+        src: "/FeatureGroupImages/Group11.png",
+        alt: "Group11",
+        description: "Group 11",
+        longDescription:
+            "Group 11 is comprised of special internal and external features found in spacer layers and some perimeter shapes. Note: 0.312\" Min diameter size. Call for consultation on anything smaller.",
+    },
+    {
+        src: "/FeatureGroupImages/Group12.png",
+        alt: "Group12",
+        description: "Group 12",
+        longDescription:
+            "Group 12 is comprised of features similar to the double D and cross-hairs. Pricing is based on similar setup and run times.",
+    },
+    {
+        src: "/FeatureGroupImages/Group17.png",
+        alt: "Group17",
+        description: "Group 17",
+        longDescription:
+            "Group 17 is comprised of special perimeter features common to spacer layers and circuit dies.",
     },
 ];
